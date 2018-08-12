eaf6cc7b9ec3fa4d45fb3da0507e91efa9bcea4c
Artemee Senin <artemee.senin@gmail.com>
2018-08-12 04:10:41 +0300

    brief cards ready

--- task-answer/src/js/main.js
+++ task-answer/src/js/main.js
@@ -1,1 +1,13 @@
-console.log("loading plugins");
+console.log("loading js");
+/* Code just for some btton enteractivity not for production.
+   For production use slider */
+let top = 0,
+    topMax = document.querySelector('.main-info__brief-cards-list').childElementCount * (-135);
+
+document.querySelector('#brief-cards-next').addEventListener('click', (e) => {
+  e.preventDefault();
+  let list = document.querySelector('.main-info__brief-cards-list');
+  top -= 135
+  if(top > topMax)
+    list.style.transform = `translateY(${top}px)`  
+})
