76d1bfa2525e43f07b4bbe45f1399fb375de680a
Artemee Senin <artemee.senin@gmail.com>
2018-08-20 20:43:41 +0300

    adaptive modals in process

--- task-answer/src/js/main.js
+++ task-answer/src/js/main.js
@@ -18,3 +18,7 @@ document.querySelector('#brief-cards-next').addEventListener('click', (e) => {
 briefCard('#main-info__brief-cards-list', '#modals-container__main-info', '/data/main-info.json', '', true);
 briefCard('#favorite-scenarios__scenarios-list', '#modals-container__favorite-scenarios', '/data/scenarios.json', 'short');
 briefCard('#favorite-devices__devices-list', '#modals-container__favorite-devices', '/data/devices.json', '', true);
+
+document.querySelector('.color-button-list__item--toggle').addEventListener('click', (e) => {
+  e.target.closest('.color-button-list').querySelector('.color-button-list-wrapper').classList.toggle('color-button-list-wrapper--show')
+})
